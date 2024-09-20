5bdd74d283d77bff1a10afb4eafb228ac6bd182f
Ben H <bensonhuang2002@berkeley.edu>
2024-09-19 17:37:29 -0700

    checking

--- muscular/fitness-time/src/components/Generator.jsx
+++ muscular/fitness-time/src/components/Generator.jsx
@@ -3,7 +3,7 @@ import SectionWrapper from './SectionWrapper'
 
 export default function Generator() {
   return (
-    <SectionWrapper header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
+    <SectionWrapper header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}> {/* the spans and \' are for exiting the ' limits*/}
 
     </SectionWrapper>
   )
